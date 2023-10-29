9cf63ae6eb1e21f81d285a1cb2f75b364febb452
MotiCAT <youyuzhangye@gmail.com>
2023-10-29 15:36:23 +0900

    fix: json being opened at the same time

--- src/commands/banword.js
+++ src/commands/banword.js
@@ -11,10 +11,6 @@ module.exports = {
                 .addStringOption(option => option.setName('text').setDescription('禁止するワードを入力してください。').setRequired(true)))
         .addSubcommand(subcommand =>
             subcommand
-                .setName('list')
-                .setDescription('禁止ワード一覧を表示します。'))
-        .addSubcommand(subcommand =>
-            subcommand
                 .setName('setting')
                 .setDescription('特定の禁止ワードの設定を行います。')
                 .addStringOption(option => option.setName('text').setDescription('設定を変更する禁止ワードを指定してください。').setRequired(true).setAutocomplete(true))
@@ -45,76 +41,123 @@ module.exports = {
         if (subcommand === 'add') {
             const text = interaction.options.getString('text')
             const serverId = interaction.guild.id;
-            const data = require(`${__dirname}/../jsons/banword.json`);
-            let serverData = data[serverId];
-            if (!serverData) {
-                serverData = {};
-                data[serverId] = serverData;
-            };
-            serverData[text] = 2;
-            fs.writeFile(`${__dirname}/../jsons/banword.json`, JSON.stringify(data, null, 2), (err) => {
-                if (err) {
-                    const errorstack = '```js\n' + err.stack + '```';
-                    interaction.reply({ embeds: [new EmbedBuilder().setTitle(`${err}`).setColor('#ff0000').setDescription(`${errorstack}\n</bugreport:1084336997973364850>でバグを報告してください。`)] })
-                } else {
-                    interaction.reply({ embeds: [new EmbedBuilder().addFields({ name: 'Done.', value: `「${text}」を禁止ワードに追加しました。` }).setColor('#3CB371').setFooter({ text: 'CH Protect 0.0.1', iconURL: interaction.client.user.displayAvatarURL() })], ephemeral: true });
+            const filePath = `${__dirname}/../jsons/banword.json`;
+            fs.readFile(filePath, 'utf8', (readErr, data) => {
+                if (readErr) {
+                    interaction.reply({ content: 'エラーが発生しました。', ephemeral: true });
+                    return;
+                }
+
+                let jsonData;
+
+                try {
+                    jsonData = JSON.parse(data);
+                } catch (parseErr) {
+                    interaction.reply({ content: 'セーブデータの解析中にエラーが発生しました。', ephemeral: true });
+                    return;
+                }
+
+                if (jsonData === null) {
+                    interaction.reply({ content: '他のプロセスによってファイルが使用中です。しばらく待ってから再試行してください。', ephemeral: true });
+                    return;
+                }
+                let serverData = data[serverId];
+                if (!serverData) {
+                    serverData = {};
+                    data[serverId] = serverData;
                 };
+                serverData[text] = 2;
+                fs.writeFile(filePath, JSON.stringify(data, null, 2), (err) => {
+                    if (err) {
+                        interaction.reply({ content: 'エラーが発生しました。', ephemeral: true });
+                    } else {
+                        interaction.reply({ embeds: [new EmbedBuilder().addFields({ name: 'Done.', value: `「${text}」を禁止ワードに追加しました。` }).setColor('#3CB371').setFooter({ text: 'CH Protect 0.0.1', iconURL: interaction.client.user.displayAvatarURL() })], ephemeral: true });
+                    };
+                });
             });
         };
         if (subcommand === 'remove') {
             const text = interaction.options.getString('text')
             const serverId = interaction.guild.id;
-            const data = require(`${__dirname}/../jsons/banword.json`);
-            const serverData = data[serverId];
-            if (!serverData || !serverData[text]) {
-                interaction.reply({ embeds: [new EmbedBuilder().addFields({ name: 'Error', value: `${text}という禁止ワードは登録されていません。` }).setColor('ff0000').setFooter({ text: 'CH Protect 0.0.1', iconURL: interaction.client.user.displayAvatarURL() })], ephemeral: true });
-                return;
-            };
-            delete serverData[text];
-            fs.writeFile(`${__dirname}/../jsons/banword.json`, JSON.stringify(data, null, 2), (err) => {
-                if (err) {
-                    const errorstack = '```js\n' + err.stack + '```';
-                    interaction.reply({ embeds: [new EmbedBuilder().setTitle(`${err}`).setColor('#ff0000').setDescription(`${errorstack}\n</bugreport:1084336997973364850>でバグを報告してください。`)] });
-                } else {
-                    interaction.reply({ embeds: [new EmbedBuilder().addFields({ name: 'Done.', value: `「${text}」を禁止ワードから除外しました。` }).setColor('#3CB371').setFooter({ text: 'CH Protect 0.0.1', iconURL: interaction.client.user.displayAvatarURL() })], ephemeral: true });
+            const filePath = `${__dirname}/../jsons/banword.json`;
+            fs.readFile(filePath, 'utf8', (readErr, data) => {
+                if (readErr) {
+                    interaction.reply({ content: 'エラーが発生しました。', ephemeral: true });
+                    return;
+                }
+
+                let jsonData;
+
+                try {
+                    jsonData = JSON.parse(data);
+                } catch (parseErr) {
+                    interaction.reply({ content: 'セーブデータの解析中にエラーが発生しました。', ephemeral: true });
+                    return;
+                }
+
+                if (jsonData === null) {
+                    interaction.reply({ content: '他のプロセスによってファイルが使用中です。しばらく待ってから再試行してください。', ephemeral: true });
+                    return;
+                }
+                const serverData = data[serverId];
+                if (!serverData || !serverData[text]) {
+                    interaction.reply({ embeds: [new EmbedBuilder().addFields({ name: 'Error', value: `${text}という禁止ワードは登録されていません。` }).setColor('ff0000').setFooter({ text: 'CH Protect 0.0.1', iconURL: interaction.client.user.displayAvatarURL() })], ephemeral: true });
+                    return;
                 };
-            });
+                delete serverData[text];
+                fs.writeFile(filePath, JSON.stringify(data, null, 2), (err) => {
+                    if (err) {
+                        interaction.reply({ content: 'エラーが発生しました。', ephemeral: true });
+                    } else {
+                        interaction.reply({ embeds: [new EmbedBuilder().addFields({ name: 'Done.', value: `「${text}」を禁止ワードから除外しました。` }).setColor('#3CB371').setFooter({ text: 'CH Protect 0.0.1', iconURL: interaction.client.user.displayAvatarURL() })], ephemeral: true });
+                    };
+                });
+            })
         };
         if (subcommand === 'setting') {
             const text = interaction.options.getString('text');
             const time = interaction.options.getInteger('time')
             const serverId = interaction.guild.id;
-            const data = require(`${__dirname}/../jsons/banword.json`);
-            const serverData = data[serverId];
-            if (!serverData || Object.keys(serverData).length === 0) {
-                interaction.reply({ embeds: [new EmbedBuilder().addFields({ name: 'Error', value: '禁止ワードの登録がありません。' }).setColor('#ff0000').setFooter({ text: 'CH Protect 0.0.1', iconURL: interaction.client.user.displayAvatarURL() })], ephemeral: true });
-                return;
-            };
-            if (!serverData[text]) {
-                interaction.reply({ embeds: [new EmbedBuilder().addFields({ name: 'Error', value: `${text}という禁止ワードは登録されていません。` }).setColor('ff0000').setFooter({ text: 'CH Protect 0.0.1', iconURL: interaction.client.user.displayAvatarURL() })], ephemeral: true });
-                return;
-            };
-            if (time < 1) return interaction.reply({ embeds: [new EmbedBuilder().addFields({ name: 'Error', value: '引数「time」には1以上の数を指定してください。' }).setColor('ff0000').setFooter({ text: 'CH Protect 0.0.1', iconURL: interaction.client.user.displayAvatarURL() })], ephemeral: true });
-            data[interaction.guild.id][text] = time;
+            const filePath = `${__dirname}/../jsons/banword.json`;
+            fs.readFile(filePath, 'utf8', (readErr, data) => {
+                if (readErr) {
+                    interaction.reply({ content: 'エラーが発生しました。', ephemeral: true });
+                    return;
+                }
 
-            const newData = JSON.stringify(data, null, 2);
+                let jsonData;
 
-            fs.writeFileSync(`${__dirname}/../jsons/banword.json`, newData);
-            await interaction.reply({ embeds: [new EmbedBuilder().addFields({ name: 'Done.', value: '設定を変更しました。' }).setColor('#3CB371').setFooter({ text: 'CH Protect 0.0.1', iconURL: interaction.client.user.displayAvatarURL() })], ephemeral: true })
-        };
-        if (subcommand === 'list') {
-            const serverId = interaction.guild.id;
-            const data = require(`${__dirname}/../jsons/banword.json`);
-            const serverData = data[serverId];
-            if (!serverData || Object.keys(serverData).length === 0) {
-                interaction.reply({ embeds: [new EmbedBuilder().addFields({ name: 'Error', value: '禁止ワードの登録がありません。' }).setColor('#ff0000').setFooter({ text: 'CH Protect 0.0.1', iconURL: interaction.client.user.displayAvatarURL() })], ephemeral: true });
-                return;
-            };
-            let banwordList = '';
-            for (const text in serverData) {
-                banwordList += `${text} : タイムアウト時間 ${serverData[text]}分\n`;
-            };
-            await interaction.reply({ embeds: [new EmbedBuilder().addFields({ name: '禁止ワード一覧', value: banwordList }).setColor('#3CB371').setFooter({ text: 'CH Protect 0.0.1', iconURL: interaction.client.user.displayAvatarURL() })], ephemeral: true });
+                try {
+                    jsonData = JSON.parse(data);
+                } catch (parseErr) {
+                    interaction.reply({ content: 'セーブデータの解析中にエラーが発生しました。', ephemeral: true });
+                    return;
+                }
+
+                if (jsonData === null) {
+                    interaction.reply({ content: '他のプロセスによってファイルが使用中です。しばらく待ってから再試行してください。', ephemeral: true });
+                    return;
+                }
+                const serverData = data[serverId];
+                if (!serverData || Object.keys(serverData).length === 0) {
+                    interaction.reply({ embeds: [new EmbedBuilder().addFields({ name: 'Error', value: '禁止ワードの登録がありません。' }).setColor('#ff0000').setFooter({ text: 'CH Protect 0.0.1', iconURL: interaction.client.user.displayAvatarURL() })], ephemeral: true });
+                    return;
+                };
+                if (!serverData[text]) {
+                    interaction.reply({ embeds: [new EmbedBuilder().addFields({ name: 'Error', value: `${text}という禁止ワードは登録されていません。` }).setColor('ff0000').setFooter({ text: 'CH Protect 0.0.1', iconURL: interaction.client.user.displayAvatarURL() })], ephemeral: true });
+                    return;
+                };
+                if (time < 1) return interaction.reply({ embeds: [new EmbedBuilder().addFields({ name: 'Error', value: '引数「time」には1以上の数を指定してください。' }).setColor('ff0000').setFooter({ text: 'CH Protect 0.0.1', iconURL: interaction.client.user.displayAvatarURL() })], ephemeral: true });
+                data[interaction.guild.id][text] = time;
+
+                fs.writeFile(filePath, JSON.stringify(data, null, 2), (err) => {
+                    if (err) {
+                        interaction.reply({ content: 'エラーが発生しました。', ephemeral: true });
+                    } else {
+                        interaction.reply({ embeds: [new EmbedBuilder().addFields({ name: 'Done.', value: `「${text}」のタイムアウト時間を${time}分に変更しました。` }).setColor('#3CB371').setFooter({ text: 'CH Protect 0.0.1', iconURL: interaction.client.user.displayAvatarURL() })], ephemeral: true });
+                    };
+                });
+            });
         };
     }
 };
